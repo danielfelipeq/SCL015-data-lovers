2d0ff7d278b0dc360fa3008aa6277b18d5e17720
nayruthCalla <nayruth.calla@gmail.com>
2021-01-24 16:44:26 -0500

    maquetación html

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/clash_royale/ClashRoyale.js';
+//const data = require('./data/clash_royale/ClashRoyale.js'); --- ES5
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(data);
